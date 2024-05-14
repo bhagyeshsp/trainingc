110d74a5a9758346a76c57add27e0194bc0533f6
Bhagyesh Pathak <bhagyeshspathak@gmail.com>
2024-05-14 20:01:00 +0530

    corrected typo from destory to destroy

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,5 +1,5 @@
 class Teacher < ApplicationRecord
-has_many :students, dependent: :destory, inverse_of: :project
+has_many :students, dependent: :destroy, inverse_of: :project
 
 accepts_nested_attributes_for :students, allow_destroy: true, reject_if: :all_blank
 end
